12ffddb7b91f0ee2ae167b0f9d1c7a73d520a3be
Clayton Stupi <clay.stup@gmail.com>
2020-09-05 12:29:48 -0700

    auth working. error on mobile but still functions. Seems to be an amplify bug. Next navigation. Add screen for buttons, site then configure site

--- App.tsx
+++ App.tsx
@@ -1,10 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native'
+import { NavigationContainer } from '@react-navigation/native';
 import Amplify from 'aws-amplify';
 import { withAuthenticator } from 'aws-amplify-react-native'
+import awsConfig from './aws.config.js'
 
-Amplify.configure(awsConfig);
+Amplify.configure({
+  Auth: {
+    region: awsConfig.REGION,
+    userPoolId: awsConfig.USER_POOL_ID,
+    userPoolWebClientId: awsConfig.USER_POOL_CLIENT_ID
+  }
+});
 
 const callGarage = (pin: number) => {
   console.log(`pin ${pin} was selected`)
@@ -13,11 +21,13 @@ export default withAuthenticator(App)
 
 function App() {
   return (
-    <View style={styles.container}>
-      <Button onPress={() => callGarage(4) } title="Trigger" />
-      <Text>Open up App.tsx to start working on your app! EDITED TEXT!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <View style={styles.container}>
+        <Button onPress={() => callGarage(4) } title="Trigger" />
+        <Text>Open up App.tsx to start working on your app! EDITED TEXT!</Text>
+        <StatusBar style="auto" />
+      </View>
+    </NavigationContainer>
   );
 }
 
